039925ab501e81a9a8a670deff775397ae5680e8
Amol Jadhav <amol@Amols-MacBook-Air.local>
2021-08-08 23:38:45 +0530

    Changed findByUsername method in UserRepository

--- starter_code/src/main/java/com/example/demo/model/persistence/repositories/UserRepository.java
+++ starter_code/src/main/java/com/example/demo/model/persistence/repositories/UserRepository.java
@@ -6,6 +6,5 @@ import com.example.demo.model.persistence.User;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	@Query("select u from User u where u.username = ?1")
 	User findByUsername(String username);
 }
